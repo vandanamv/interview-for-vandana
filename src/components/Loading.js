3c49e6692d0050cfef9c06adb2ea8aabf7d5be19
Vandanamv <vandanareddymv@gmail.com>
2025-07-24 19:41:15 +0530

    Changed Ui of Loading spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 
 const Loading = () => {
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="400px">
-      <CircularProgress />
-      <Typography variant="body1" color="text.secondary" mt={2}>
-        Loading launches...
-      </Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '400px',
+      }}
+    >
+      <CircularProgress
+        sx={{
+          color: '#1976d2', 
+        }}
+      />
     </Box>
   );
 };
